318b4f074e395d2107b375cac438fcf386235df6
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-10-01 18:33:35 +0100

    First implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser};
 use reqwest;
+use dotenv;
+use serde::Deserialize;
 
 const LAT: f32 = -41.2;
 const LON: f32 = 174.7;
@@ -13,8 +15,63 @@ struct Args {
     days: u8,
 }
 
-fn main() {
-    let args: Args = Args::parse();
+#[derive(Deserialize, Debug)]
+struct Coord {
+    lat: f32,
+    lon: f32,
+}
 
-    println!("{}", args.days);
+#[derive(Deserialize, Debug)]
+struct Weather {
+    id: u32,
+    main: String,
+    description: String,
+    icon: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct CurrentWeatherMain {
+    temp: f32,
+    feels_like: f32,
+}
+
+#[derive(Deserialize, Debug)]
+struct CurrentWeather {
+    coord: Coord,
+    weather: Vec<Weather>,
+    // base: CurrentWeatherMain
+    main: CurrentWeatherMain
+}
+
+fn main() -> Result<(), reqwest::Error> {
+    dotenv::dotenv().ok();
+
+    let mut api_key: Option<String> = None;
+    for (key, value) in std::env::vars() {
+        if key != "APIKEY" {
+            continue;
+        }
+    }
+    if api_key.is_none() {
+        panic!("need API key");
+    }
+    let api_key = api_key.unwrap();
+
+    let args: Args = Args::parse();
+    
+    let method = match args.days {
+        0 => "weather",
+        _ => "forecasting"
+    };
+
+    let cnt = args.days * 8;
+
+    let url = format!("https://api.openweathermap.org/data/2.5/forecast?lat={LAT}&lon={LON}&appid={api_key}&units=metric&cnt={cnt}");
+
+    let weather: CurrentWeather = reqwest::blocking::get(url)?
+    .json()?;
+
+    println!("{} {:?}",weather.main.temp, weather.weather[0].description);
+
+    Ok(())
 }
